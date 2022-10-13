4c91527601b97e9a8cd488724be15a9dde0738d1
carloshs1 <carlos@ebless.mx>
2022-10-13 03:46:31 -0500

    Add function to shuffle breeds

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { shuffleBreeds } from '../utils/functions'
 import { mockBreeds } from '../utils/mock'
 import { breedsType } from '../utils/types'
 import Story from './Story'
@@ -13,7 +14,10 @@ const Stories: React.FC = () => {
  }, [])
  return (
   <div className="flex space-x-2 mx-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black md:max-w-3xl">
-   {breeds && breeds.map((breed) => <Story key={breed.id} breed={breed} />)}
+   {breeds &&
+    shuffleBreeds(breeds).map((breed) => (
+     <Story key={breed.id} breed={breed} />
+    ))}
   </div>
  )
 }
